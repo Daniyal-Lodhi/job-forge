d1601ab242c4a71dde261bc021b27013cfce6be1
Daniyal Lodhi <daniyallodhi25@gmail.com>
2023-10-09 00:08:03 +0500

    search api added

---
+++ src/app/api/jobs/searchJobs/[searchTerm]/route.js
@@ -0,0 +1,20 @@
+import Jobs from "@/app/lib/models/jobs";
+import { NextResponse } from "next/server";
+
+
+export const GET = async(req,content)=>{
+    var success ;
+    try {
+        var searchTerm = content.params.searchTerm ;
+        const regexSearchTerm = new RegExp(searchTerm,"i") 
+        let jobs = await Jobs.find({jobTitle:regexSearchTerm})
+
+
+        success = true ;
+        return NextResponse.json({success ,jobs} , {status:200})
+    } catch (error) {
+        success = false
+        return NextResponse.json({success , error:error.message} , {status:500})
+
+    } 
+}
